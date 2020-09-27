5456e75c6adbc68f9acdaf395e2da8ce0ee47a08
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-27 14:44:42 -0400

    Add About text

--- client/components/About.js
+++ client/components/About.js
@@ -5,7 +5,19 @@ const About = () => {
   return (
     <Section>
       <div className="content">
-        <p>This app was created because...!</p>
+        <p className="block">
+          As of Sept, 2020, seven months since Pennsylvania's first reported
+          case of COVID-19, the Pa. Department of Health continues to make it
+          difficult for journalists and researchers to easily access statistics
+          it compiles daily on new cases, deaths, and negative tests.
+        </p>
+        <p className="block">
+          The department uploads a PDF most days of the week with running totals
+          of these statistics. Because of the nature of PDFs, it can be awkward
+          to extract this data quickly and easily. This app is designed to make
+          it easier for researchers and journalists to convert these PDFs to
+          plain text.
+        </p>
       </div>
     </Section>
   );
