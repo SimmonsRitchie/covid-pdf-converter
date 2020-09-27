2c9cdbaf4afc7c006b8859cf2fed36effd077ce5
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-27 12:44:24 -0400

    Add new tests, set location of dummy data to PDFs saved in s3

--- server/tests/test_scrape.py
+++ server/tests/test_scrape.py
@@ -2,17 +2,23 @@ import pytest
 from ..scrape import scrape_pdfs
 
 
-def test_scrape_runs_sept_25():
-    cases_url_sept_25 = "https://www.health.pa.gov/topics/Documents/Diseases%20and%20Conditions/COVID-19%20County%20Data/County%20Case%20Counts_9-25-2020.pdf"
-    deaths_url_sept_25 = "https://www.health.pa.gov/topics/Documents/Diseases%20and%20Conditions/COVID-19%20Death%20Data/Death%20by%20County%20of%20Residence%20--%202020-09-25.pdf"
+def test_scrape_runs_sept_19():
+    cases_url = "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-cases__2020-09-19.pdf"
+    deaths_url = "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-deaths__2020-09-19.pdf"
+    result = scrape_pdfs(cases_url, deaths_url)
+    print(result)
 
-    result = scrape_pdfs(cases_url_sept_25, deaths_url_sept_25)
+
+def test_scrape_runs_sept_25():
+    cases_url = "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-cases__2020-09-25.pdf"
+    deaths_url = "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-deaths__2020-09-25.pdf"
+    result = scrape_pdfs(cases_url, deaths_url)
     print(result)
 
 
 def test_scrape_runs_sept_26():
-    cases_url_sept_25 = "https://www.health.pa.gov/topics/Documents/Diseases%20and%20Conditions/COVID-19%20County%20Data/County%20Case%20Counts_9-26-2020.pdf"
-    deaths_url_sept_25 = "https://www.health.pa.gov/topics/Documents/Diseases%20and%20Conditions/COVID-19%20Death%20Data/Death%20by%20County%20of%20Residence%20--%202020-09-26.pdf"
+    cases_url = "https://www.health.pa.gov/topics/Documents/Diseases%20and%20Conditions/COVID-19%20County%20Data/County%20Case%20Counts_9-26-2020.pdf"
+    deaths_url = "https://www.health.pa.gov/topics/Documents/Diseases%20and%20Conditions/COVID-19%20Death%20Data/Death%20by%20County%20of%20Residence%20--%202020-09-26.pdf"
 
-    result = scrape_pdfs(cases_url_sept_25, deaths_url_sept_25)
+    result = scrape_pdfs(cases_url, deaths_url)
     print(result)
