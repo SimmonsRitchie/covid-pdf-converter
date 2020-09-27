d54dd23ea76eedfb03f83fbd4d49b93d9b43c433
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-27 13:35:55 -0400

    Update demo URLs

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const SAMPLE_CASES_URL =
-  "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-cases__2020-09-25.pdf";
+  "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-cases__2020-09-26.pdf";
 export const SAMPLE_DEATHS_URL =
-  "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-deaths__2020-09-25.pdf";
+  "https://interactives.data.spotlightpa.org/assets/covid-pdf-converter/sample/sample__county-deaths__2020-09-26.pdf";
 export const TAB_SCHEMA = [
   { id: "table", label: "Table", faIcon: "fas fa-table" },
   { id: "text", label: "Text", faIcon: "fas fa-align-justify" },
